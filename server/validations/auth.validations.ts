93eabdedec04b5f89c9238ac43c67d1b811218ea
Shaptorshi Bhattacharya <shaptorshib@gmail.com>
2026-02-27 18:22:16 +0530

    Regex

--- server/validations/auth.validations.ts
+++ server/validations/auth.validations.ts
@@ -3,7 +3,7 @@ import {z} from 'zod';
 export const registerSchema = z.object({
     name:z.string().min(3,"Name must contain atleast 3 characters!"),
     email:z.email("Invalid email format"),
-    password:z.string().min(8,"Create a password of minimum 8 characters!"),
+    password:z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@_$])[A-Za-z\d@_$]{8,}$/,"Password must be at least 8 characters, include uppercase, lowercase, number and special characters"),
 })
 
 export const loginSchema  =z.object({
